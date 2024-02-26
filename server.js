6f57c609de0c79395c08b51eedb925a1ed2023b7
Arthit LungYa <rthit096@gmail.com>
2024-02-26 13:08:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 const mysql = require('mysql2');
 require('dotenv').config()
 
 const app = express();
 
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
