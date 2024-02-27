0f84727043d69320b51caa4af6e1574ad1efd386
ArthitDev <rthit096@gmail.com>
2024-02-28 03:13:12 +0700

    update-api

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function processComments(games, res) {
 
 app.post('/games', (req, res) => {
     const { game_name, game_description, img, game_type, game_id, comment, vote, username } = req.body;
-    if (!game_id) {
+    if (game_id !== undefined) {
         const commentSql = "INSERT INTO games_comments (game_id, comment, vote, username) VALUES (?, ?, ?, ?)";;
         connection.query(commentSql, [game_id, comment, vote, username], (err) => {
             if (err) {
@@ -104,7 +104,7 @@ app.post('/games', (req, res) => {
             res.json({ status: 'success', message: 'Comment data inserted successfully' });
         });
     }
-    else {
+    else if (game_id === undefined) {
         const gameSql = "INSERT INTO games (game_name, game_description, img, game_type) VALUES (?, ?, ?, ?)";
         connection.query(gameSql, [game_name, game_description, img, game_type], (err) => {
             if (err) {
