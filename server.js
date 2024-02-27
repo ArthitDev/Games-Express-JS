695e6d9ae2c3b3ec63764e9d7644e18acc77a9d5
ArthitDev <rthit096@gmail.com>
2024-02-28 03:30:22 +0700

    update-api

--- server.js
+++ server.js
@@ -93,27 +93,15 @@ function processComments(games, res) {
 }
 
 app.post('/games', (req, res) => {
-    const { game_name, game_description, img, game_type, game_id, comment, vote, username } = req.body;
-    if (game_id !== undefined) {
-        // const commentSql = "INSERT INTO games_comments (game_id, comment, vote, username) VALUES (?, ?, ?, ?)";;
-        // connection.query(commentSql, [game_id, comment, vote, username], (err) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return res.status(500).json({ status: 'error', message: 'ตรงนี้เข้า Error บรรทัด 102 Comment ไม่เข้า' });
-        //     }
-        //     res.json({ status: 'success', message: 'Comment data inserted successfully' });
-        // });
-    }
-    else {
-        const gameSql = "INSERT INTO games (game_name, game_description, img, game_type) VALUES (?, ?, ?, ?)";
-        connection.query(gameSql, [game_name, game_description, img, game_type], (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ status: 'error', message: 'ตรงนี้เข้า Error บรรทัด 112 เพิ่มเกม ไม่เข้า' });
-            }
-            res.json({ status: 'success', message: 'Game data inserted successfully' });
-        });
-    }
+    const { game_name, game_description, img, game_type } = req.body;
+    const gameSql = "INSERT INTO games (game_name, game_description, img, game_type) VALUES (?, ?, ?, ?)";
+    connection.query(gameSql, [game_name, game_description, img, game_type], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ status: 'error', message: 'ตรงนี้เข้า Error บรรทัด 101 เพิ่มเกม ไม่เข้า' });
+        }
+        res.json({ status: 'success', message: 'Game data inserted successfully' });
+    });
 });
 
 
