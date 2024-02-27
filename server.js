50d401f01ca02885a586935b7b9b0f008a2cf8d4
ArthitDev <rthit096@gmail.com>
2024-02-28 04:08:11 +0700

    update-api

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://react-games-voting.vercel.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
