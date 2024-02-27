a98a849e216dd40bc3fb832673174c0f2ff31fb4
ArthitDev <rthit096@gmail.com>
2024-02-28 04:20:20 +0700

    update-api

--- server.js
+++ server.js
@@ -7,16 +7,11 @@ require('dotenv').config()
 const app = express();
 
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(bodyParser.json());
 
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
