a357cb70d898500cd3f1917c51ecab0501d8b57d
Arthit LungYa <rthit096@gmail.com>
2024-02-28 22:07:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const app = express();
 app.use(bodyParser.json());
 
 // app.use(cors())
-const corsOptions = {
-    origin: ['https://react-games-voting.vercel.app', 'https://react-games-voting.vercel.app/dashboard'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-};
+
+var corsOptions = function(req, res, next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 
+    'Content-Type, Authorization, Content-Length, X-Requested-With');
+     next();
+}
+
+app.use(corsOptions);
 
 app.use(cors(corsOptions));
 
